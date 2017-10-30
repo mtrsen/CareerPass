c1ce450f5815af2aae3ea459867976919efc15a5
hli494 <hli494@gatech.edu>
2017-10-29 21:27:36 -0400

    addbutton

--- app/src/main/java/com/rainbowsix/careerpass/SearchActivity.java
+++ app/src/main/java/com/rainbowsix/careerpass/SearchActivity.java
@@ -1,8 +1,13 @@
 package com.rainbowsix.careerpass;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,9 +17,12 @@ public class SearchActivity extends MenuActivity {
     SearchView searchView;
     ListView listView;
     SearchAdapter searchAdapter;
+    Button search;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         super.onCreate(savedInstanceState);
         getLayoutInflater().inflate(R.layout.activity_search, frameLayout);
         initialize();
@@ -25,10 +33,18 @@ public class SearchActivity extends MenuActivity {
         searchView = (SearchView)findViewById(R.id.searchView);
         listView = (ListView)findViewById(R.id.listview);
         searchAdapter = new SearchAdapter(SearchActivity.this, data);
-        data.add(new ListSingle("Prepare for interviews", "20170303", "Interview", false));
-        data.add(new ListSingle("Participate interview questions", "20180303", "Resume", false));
-        data.add(new ListSingle("Participate in oncampus interviews", "20190303", "Interview", false));
+        search = (Button)findViewById(R.id.search);
+
         listView.setAdapter(searchAdapter);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                data.add(new ListSingle("Prepare for interviews", "20170303", "Interview", false));
+                data.add(new ListSingle("Participate interview questions", "20180303", "Resume", false));
+                data.add(new ListSingle("Participate in oncampus interviews", "20190303", "Interview", false));
+                searchAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
 
