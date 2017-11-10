66eed741865de38d46299d3097b16fc47e0e20fa
Neptunennn <guoyuanwu@gatech.edu>
2017-11-10 15:14:20 -0500

    go

--- app/src/main/java/com/rainbowsix/careerpass/GraphActivity.java
+++ app/src/main/java/com/rainbowsix/careerpass/GraphActivity.java
@@ -1,20 +1,61 @@
 package com.rainbowsix.careerpass;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Combo{
+    int num;
+    String time;
+    public Combo(int num, String time) {
+        this.num = num;
+        this.time = time;
+    }
+}
 public class GraphActivity extends AppCompatActivity {
-
+    List<Combo> tagData;
     GraphView graph;
-
+    String tagName;
+    DatabaseReference databaseReference;
+    Map<String, Combo> map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        tagData = new ArrayList<>();
+        map = new HashMap<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                DataSnapshot snap = dataSnapshot.child("post");
+                for(DataSnapshot post : snap.getChildren()){
+                    for(DataSnapshot category : post.getChildren()) {
+                        for(DataSnapshot tag : category.getChildren()) {
+
+                        }
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         graph = (GraphView)findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
